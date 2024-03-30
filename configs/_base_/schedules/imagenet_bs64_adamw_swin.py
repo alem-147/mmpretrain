65f85f340820a0fef5a83275d07c6b8e3b454d1a
alem-147 <atsm360@gmail.com>
2024-03-29 19:09:32 -0500

    training swin tiny on 75 epochs

--- configs/_base_/schedules/imagenet_bs64_adamw_swin.py
+++ configs/_base_/schedules/imagenet_bs64_adamw_swin.py
@@ -38,4 +38,4 @@ test_cfg = dict()
 
 # NOTE: `auto_scale_lr` is for automatically scaling LR,
 # based on the actual training batch size.
-auto_scale_lr = dict(base_batch_size=64)
+auto_scale_lr = dict(base_batch_size=1024)
